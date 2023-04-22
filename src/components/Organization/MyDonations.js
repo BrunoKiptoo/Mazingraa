1a74372e449b8298c3a82059b2b9fb507ca65e7e
Bruno <brunochelimo@gmail.com>
2023-04-22 20:51:17 +0300

    added account settings component

--- src/components/Organization/MyDonations.js
+++ src/components/Organization/MyDonations.js
@@ -1,61 +1,5 @@
 
 
-import React, { useEffect, useState } from 'react';
-
-function MyDonations() {
-  const [donations, setDonations] = useState([]);
-
-  useEffect(() => {
-    async function fetchDonations() {
-      const response = await fetch('  http://localhost:5000/donations', { method: 'GET' });
-      const data = await response.json();
-      setDonations(data);
-    }
-
-    fetchDonations();
-  }, []);
-
-  const totalDonations = donations.reduce((acc, curr) => acc + curr.amount, 0);
-
-  return (
-    <div className="bg-gray-200 p-4">
-      <div className="bg-white shadow rounded-lg p-4 mb-4">
-        <div className="font-bold text-lg mb-2">Total Donations Received:</div>
-        <div className="font-bold text-2xl">${totalDonations}</div>
-      </div>
-      <div className="flex flex-wrap -mx-4">
-        {donations.map(donation => (
-          <div key={donation.id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
-            <div className="bg-yellow-400 shadow rounded-lg reminder-card">
-              <div className="p-4">
-                <div className="font-bold text-lg">{donation.amount}</div>
-                <div className="text-gray-500">{donation.time}</div>
-              </div>
-              <div className="px-4 pb-4">
-                <div className="flex items-center">
-                  <img className="w-8 h-8 rounded-full mr-2" src={donation.donorImage} alt={donation.donorName} />
-                  <div>
-                    <div className="font-bold">{donation.anonymous ? 'Anonymous' : donation.donorName}</div>
-                    <div className="text-sm text-gray-500">{donation.donorEmail}</div>
-                  </div>
-                </div>
-                <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert(`Thank you, ${donation.donorName}!`)}>
-                  Say Thanks
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default MyDonations;
-
-
-
-
 // import React, { useEffect, useState } from 'react';
 
 // function MyDonations() {
@@ -63,7 +7,7 @@ export default MyDonations;
 
 //   useEffect(() => {
 //     async function fetchDonations() {
-//       const response = await fetch('http://localhost:5000/donations', { method: 'GET' });
+//       const response = await fetch('  http://localhost:5000/donations', { method: 'GET' });
 //       const data = await response.json();
 //       setDonations(data);
 //     }
@@ -71,31 +15,32 @@ export default MyDonations;
 //     fetchDonations();
 //   }, []);
 
-//   const totalDonationAmount = donations.reduce((acc, curr) => acc + curr.amount, 0);
+//   const totalDonations = donations.reduce((acc, curr) => acc + curr.amount, 0);
 
 //   return (
 //     <div className="bg-gray-200 p-4">
-//       <div className="flex justify-between mb-4">
-//         <h2 className="text-xl font-bold">Total Donations Received:</h2>
-//         <div className="text-2xl font-bold">${totalDonationAmount}</div>
+//       <div className="bg-white shadow rounded-lg p-4 mb-4">
+//         <div className="font-bold text-lg mb-2">Total Donations Received:</div>
+//         <div className="font-bold text-2xl">${totalDonations}</div>
 //       </div>
 //       <div className="flex flex-wrap -mx-4">
 //         {donations.map(donation => (
 //           <div key={donation.id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
-//             <div className="bg-yellow-400 shadow rounded-lg">
+//             <div className="bg-white shadow rounded-lg reminder-card">
 //               <div className="p-4">
-//                 <div className="font-bold text-lg">${donation.amount}</div>
+//                 <div className="font-bold text-lg">{donation.amount}</div>
 //                 <div className="text-gray-500">{donation.time}</div>
 //               </div>
 //               <div className="px-4 pb-4">
 //                 <div className="flex items-center">
-//                   <img className="w-8 h-8 rounded-full mr-2" src={donation.donorImage} alt={donation.donorName} />
+//                   <img className="w-8 h-8 rounded-full mr-2 " src={donation.donorImage} alt={donation.donorName} />
 //                   <div>
-//                     <div className="font-bold">{donation.anonymous ? 'Anonymous' : donation.donorName}</div>
-//                     <div className="text-sm text-gray-500">{donation.donorEmail}</div>
+//                     <div className="font-bold ">{donation.anonymous ? 'Anonymous' : donation.donorName}</div>
+//                     <div className="text-sm text-gray-500 mb-4">{donation.donorEmail}</div>
 //                   </div>
 //                 </div>
-//                 <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert(`Thank you, ${donation.donorName}!`)}>
+//                 <button className="py-2 px-4 bg-gradient-to-r from-[#8f7300] to-[#c4a300] hover:from-[#c4a300] hover:to-[#8f7300] focus:ring-[#8f7300] focus:ring-offset-[#c4a300] text-white inline-flex justify-center rounded-md border border-transparent shadow-sm font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+//                  onClick={() => alert(`Thank you, ${donation.donorName}!`)}>
 //                   Say Thanks
 //                 </button>
 //               </div>
@@ -109,3 +54,61 @@ export default MyDonations;
 
 // export default MyDonations;
 
+
+
+
+import React, { useEffect, useState } from 'react';
+
+function MyDonations() {
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    async function fetchDonations() {
+      const response = await fetch('http://localhost:5000/donations', { method: 'GET' });
+      const data = await response.json();
+      setDonations(data);
+    }
+
+    fetchDonations();
+  }, []);
+
+  const totalDonations = donations.reduce((acc, curr) => acc + curr.amount, 0);
+
+  return (
+    <div className="bg-gray-400 p-4">
+      <div className="bg-white shadow rounded-lg p-4 mb-4">
+        <div className="font-bold text-lg mb-2">Total Donations Received:</div>
+        <div className="font-bold text-2xl">${totalDonations}</div>
+      </div>
+      <div className="flex flex-wrap -mx-4">
+        {donations.map(donation => (
+          <div key={donation.id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+            <div className="bg-white shadow rounded-lg reminder-card">
+              <div className="p-4">
+                <div className="font-bold text-lg">{donation.amount}</div>
+                <div className="text-gray-500">{donation.time}</div>
+              </div>
+              <div className="px-4 pb-4">
+                <div className="flex items-center mb-2">
+                  <img className="w-8 h-8 rounded-full mr-2" src={donation.donorImage} alt={donation.donorName} />
+                  <div>
+                    <div className="font-bold">{donation.anonymous ? 'Anonymous' : donation.donorName}</div>
+                    <div className="text-sm text-gray-500">{donation.donorEmail}</div>
+                  </div>
+                </div>
+                <button
+                  className="py-2 px-4 bg-gradient-to-r from-[#8f7300] to-[#c4a300] hover:from-[#c4a300] hover:to-[#8f7300] focus:ring-[#8f7300] focus:ring-offset-[#c4a300] text-white inline-flex justify-center rounded-md border border-transparent shadow-sm font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+                  onClick={() => alert(`Thank you, ${donation.donorName}!`)}
+                >
+                  Say Thanks
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MyDonations;
